2b87ca9abc1ee5f50bc526581ddb577b24c8fdea
krishan kant Tiwari <39799805+krishna23tiwari@users.noreply.github.com>
2024-10-25 23:34:53 +0530

    Add files via upload

---
+++ arrayex.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[30];
+    printf("enter the array for reversal \n");
+    gets(str);
+    printf("\n");
+
+    puts(strrev(str));
+    printf("\n");
+
+
+    puts(strrev(str));
+
+
+    return 0;
+}
